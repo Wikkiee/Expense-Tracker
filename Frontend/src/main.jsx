cae1908659129e52c79c4cce0cd65dd2fae2a714
Wikkie <vigneshrgm962950@gmail.com>
2023-07-28 19:08:00 +0530

    fixed build bug- .css isn't loaded

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthenticationProvider } from "./context/AuthenticationContext.jsx";
 import App from "./App.jsx";
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
